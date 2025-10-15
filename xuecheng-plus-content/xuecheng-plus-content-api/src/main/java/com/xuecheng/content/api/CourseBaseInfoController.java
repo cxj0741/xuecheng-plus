a6f576c9331bb5f3ef2b3abc4c35e388922a41bd
cxj <2496819537@qq.com>
2025-10-15 18:42:15 +0800

    解决swagger依赖和spring boot版本不匹配的问题

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -4,16 +4,20 @@ import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.QueryCourseParamsDto;
 import com.xuecheng.content.model.po.CourseBase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController// 相当于controller和responsebody
+@RestController
 @RequestMapping("/course")
+@Tag(name = "课程信息管理", description = "课程基础信息的增删改查")
 public class CourseBaseInfoController {
 
-    @RequestMapping("/list")
+    @Operation(summary = "课程分页查询", description = "根据条件分页查询课程信息")
+    @PostMapping("/list")
     public PageResult<CourseBase> list(PageParams pageParams,@RequestBody(required = false) QueryCourseParamsDto queryCourseParamsDto){
         return null;
     }
